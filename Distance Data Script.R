cb95de0d8ed2a37ad0163b1c61d8dbfb74dfb58e
David Terner <davidmterner@gmail.com>
2019-11-22 18:05:31 -0500

    Created a new (great circle) distance function.

--- Distance Data Script.R
+++ Distance Data Script.R
@@ -1,1 +1,28 @@
 # Working with Distance data
+setwd("/Users/davidterner/Desktop/Indiana/Year 3/S610/Final Project")
+library(readr)
+library(ggplot2)
+NBER_distance <- read_csv("NBER_distance.csv")
+
+num_coun=length(unique(NBER_distance$county1))
+
+
+
+
+
+gcd.hf <- function(long1, lat1, long2, lat2) {
+  #closely following: https://www.r-bloggers.com/great-circle-distance-calculations-in-r/
+  R <- 3958.8 # Earth mean radius in miles 
+  deg2rad <- function(deg) return(deg*pi/180)
+  long1=deg2rad(long1)
+  long2=deg2rad(long2)
+  lat1=deg2rad(lat1)
+  lat2=deg2rad(lat2)
+  delta.long <- (long2 - long1)
+  delta.lat <- (lat2 - lat1)
+  a <- sin(delta.lat/2)^2 + cos(lat1) * cos(lat2) * sin(delta.long/2)^2
+  c <- 2 * asin(min(1,sqrt(a)))
+  d = R * c
+  return(d) # Distance in miles
+}
+
