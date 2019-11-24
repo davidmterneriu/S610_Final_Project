ee22b00ff6db05b6bbca6269a9c7ef2f26bbc48e
David Terner <davidmterner@gmail.com>
2019-11-23 19:19:57 -0500

    First attempt at coding a great circle distance function, distance matrix function, and Moran's I.  Moran's I Script is now effectively used as a test script.

--- Distance Data Script.R
+++ Distance Data Script.R
@@ -1,14 +1,11 @@
 # Working with Distance data
+
 setwd("/Users/davidterner/Desktop/Indiana/Year 3/S610/Final Project")
 library(readr)
 library(ggplot2)
-NBER_distance <- read_csv("NBER_distance.csv")
-
-num_coun=length(unique(NBER_distance$county1))
-
-
-
-
+library(tidyverse)
+library(readxl)
+library(housingData)
 
 gcd.hf <- function(long1, lat1, long2, lat2) {
   #closely following: https://www.r-bloggers.com/great-circle-distance-calculations-in-r/
@@ -25,4 +22,89 @@ gcd.hf <- function(long1, lat1, long2, lat2) {
   d = R * c
   return(d) # Distance in miles
 }
+gcd.hf=Vectorize(gcd.hf)
+
+
+distance_matrix=function(geo,long,lat){
+  #Variable description-----------------------------------
+  #INPUTS:
+  #geo: list of geographic id units (index g), length n
+  #long/lat: coordinates of g in degrees 
+  #OUTPUT:
+  #nxn square and symmetric distance matrix 
+  master_df=cbind.data.frame(geo,long,lat)%>%
+    unique.data.frame()
+  geo=master_df$geo
+  long=master_df$long
+  lat=master_df$lat
+  n=length(geo)
+  test_df=expand.grid(geo,geo)%>%as.data.frame()
+  test_df=test_df%>%inner_join(master_df,by=c("Var1"="geo"))%>%
+    rename("long1"="long","lat1"="lat")%>%
+    inner_join(master_df,by=c("Var2"="geo"))%>%
+    rename("long2"="long","lat2"="lat")
+  test_df$dist=gcd.hf(test_df$long1,test_df$lat1,test_df$long2,test_df$lat2)
+  #Making the square matrix 
+  myMat <- matrix(0, n, n, dimnames = list(geo, geo))
+  # fill in the matrix with matrix indexing on row and column names
+  myMat[as.matrix(test_df[c("Var1", "Var2")])] <- test_df[["dist"]]
+  return(myMat)
+}
+
+moranI=function(geo,long,lat,y,dmax){
+  #Variable description-----------------------------------
+  #INPUTS:
+  #geo: list of geographic id units (index g)
+  #long/lat: coordinates of g in degrees 
+  #y: variable of interest
+  #dmax: max distance 
+  #OUTPUT: 
+  #moran: moran's I (e.g. distance weighted correlation)
+  #zi: z-score
+  #p.value.tw: two-sided p-value
+
+  #Create square distance matrix given long/lat coordinates
+  w_mat=distance_matrix(geo,long,lat)
+  w_mat=w_mat^(-1)
+  diag(w_mat) <- 0
+  w_mat[w_mat<(dmax)^(-1)]=0
+  W=sum(w_mat)
+  N=length(geo)
+  ymean=mean(y)
+  #Create C_ij matrix
+  C=matrix(0,N,N)
+  for(i in 1:N){
+    for(j in 1:N){
+      C[i,j]=(y[i]-ymean)*(y[j]-ymean)
+    }
+  }
+  moran=N*sum(w_mat*C)/(W*sum((y-ymean)^2))
+  
+  #Time to get z-value
+  EI=-1/(N-1)
+  S0=W
+  S1=sum((w_mat+t(w_mat))^2)
+  S2=sum((colSums(w_mat)+rowSums(w_mat))^2)
+  S3=(1/N*sum((y-ymean)^4))/(1/N*sum((y-ymean)^2)^2)
+  S4=(N^2-3*N+3)*S1+N*S2+3*W^2
+  S5=(N^2-N)*S1
+  VI=(N*S4-S3*S5)/((N-1)*(N-2)*(N-3)*W^2)-(EI)^2
+  
+  zi=(moran-EI)/sqrt(VI)
+  p.value.tw=2*(1-pnorm(zi))
+  #result=cbind.data.frame(moranI=moran,zscore=zi,p.value.tw)
+  #result=data.frame(moran=moran,p.value=p.value.tw)
+  result=moran
+  return(result)
+}
+moranI=Vectorize(moranI,vectorize.args="dmax")
+
+
+
+
+
+
+
+
+
 
--- Moran's I Script..R
+++ Moran's I Script..R
@@ -1,1 +1,83 @@
 #Coding Moran's I 
+rm(list=ls())
+setwd("/Users/davidterner/Desktop/Indiana/Year 3/S610/Final Project")
+library(readr)
+library(ggplot2)
+library(tidyverse)
+library(readxl)
+library(housingData)
+source("Distance Data Script.R")
+
+#Last library has median geographical coordinates of centroids of U.S. counties
+county_data=geoCounty
+county_data$fips=gsub("^[0]+","",county_data$fips)%>%as.character()
+Op_data <- read_excel("Op_data.xlsx")
+Op_data$FIPS=as.character(Op_data$FIPS)
+
+#Test Data
+#test_df=county_data[1:200,]
+
+sample_count=200
+
+test_df=sample_n(county_data, 200)
+#test_df=county_data[county_data$state=="NY",]
+
+test_df=test_df%>%inner_join(select(Op_data,FIPS,`Opioid Prescribing Rate`,Year),by=c("fips"="FIPS"))%>%
+  rename(OPR=`Opioid Prescribing Rate`)
+
+#Test Sequence
+dist_seq=seq(20,200,by=5)
+year_seq=2013:2017
+
+m_length=length(dist_seq)*length(year_seq)
+
+result_df=matrix(0,m_length,3)
+counter=1
+for (i in 1:length(year_seq)){
+  temp_df=test_df[test_df$Year==year_seq[i],]
+  for (j in 1:length(dist_seq)){
+    result_df[counter,1]=year_seq[i]
+    result_df[counter,2]=moranI(temp_df$fips,temp_df$lon,temp_df$lat,temp_df$OPR,dist_seq[j])
+    result_df[counter,3]=dist_seq[j]
+    counter=counter+1
+  }
+}
+
+result_df=as.data.frame(result_df)
+colnames(result_df)=c("Year","MoranI","Dmax")
+
+ggplot(data = result_df,aes(x=Dmax,y=MoranI,color=as.factor(Year)))+geom_line()+
+  theme_bw()+
+  labs(x="Distance",y="Moran's I",color="Year",title="Random Sample")
+
+
+#Misc--------
+
+#county_data$count_code=paste(county_data$county,county_data$state,sep="-")
+#county_list=unique(county_data$count_code)
+#n=length(county_list)
+
+#Generate Unique County Pairs
+#counter=0
+#county_pairs=matrix(0,n*(n-1)/2,2)
+
+#for (i in 1:(n-1)){
+#  for(j in (i+1):n){
+#    counter=counter+1
+#    county_pairs[counter,1]=county_list[i]
+#    county_pairs[counter,2]=county_list[j]
+#    }
+#  }
+#county_pairs=as.data.frame(county_pairs)
+#names(county_pairs)=c("coun.x","coun.y")
+
+#county_pairs=county_pairs%>%
+#  inner_join(select(county_data,count_code,fips,lon,lat),by=c("coun.x"="count_code"))
+#county_pairs=county_pairs%>%
+#  inner_join(select(county_data,count_code,fips,lon,lat),by=c("coun.y"="count_code"))
+
+#county_pairs=county_pairs%>%mutate(distance=gcd.hf(lon.x,lat.x,lon.y,lat.y))
+
+
+#hist(county_pairs$distance)
+#quantile(county_pairs$distance,seq(0,1,by=1/10))
